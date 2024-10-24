8d7d246091c269dc66e9d7ce645591401637cd6b
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-10-24 13:20:09 -0700

    Update CarController.cs

--- MegaKill-ULTRA v4/Assets/Scripts/CarController.cs
+++ MegaKill-ULTRA v4/Assets/Scripts/CarController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
-{
+{ 
+
     public float horzInput;
     public float vertInput;
     float currentBrakeForce;
