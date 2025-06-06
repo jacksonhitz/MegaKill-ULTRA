94ad0a7a0e00dac3b39118abff6e9ec88b022262
jacksonhitz <jacksonhitz@gmail.com>
2025-06-06 11:44:26 -0700

    fix

--- MegaKill-ULTRA v4/Assets/Scripts/Managers/VarManagers/DialogueManager.cs
+++ MegaKill-ULTRA v4/Assets/Scripts/Managers/VarManagers/DialogueManager.cs
@@ -16,8 +16,8 @@ public class DialogueManager : MonoBehaviour
 
     public void PlayRandomLine()
     {
-        var matching = System.Array.FindAll(lines, d => d.trip == TripManager.Instance.trip);
-        string line = matching[Random.Range(0, matching.Length)].line;
-        Dialogue.Instance.TypeText(line, 5f);
+       // var matching = System.Array.FindAll(lines, d => d.trip == TripManager.Instance.trip);
+       // string line = matching[Random.Range(0, matching.Length)].line;
+       // Dialogue.Instance.TypeText(line, 5f);
     }
 }
